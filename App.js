71553adf00d08b988434f01b022fed6b35b9fe00
stevekutz <stkutz@gmail.com>
2021-01-04 21:32:04 -0600

    added handler for input and rendered list of goal inputs

--- App.js
+++ App.js
@@ -1,132 +1,166 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View , TextInput, Button} from 'react-native';
 
 
 export default function App() {
-  return (
-    <View style = {styles.screen}>
-        <View style = {styles.topContainer}>
-            <TextInput
-                placeholder = "Course Goal"
-                style = {styles.input} 
-            />
-            <Button 
-                title = "button here"
-            />
-        </View>
-        <View />
+
+	const [enteredGoal, setEnteredGoal] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	// // another way
+	// function goalInputHandler (enteredText) {
+	// 	console.log(enteredText)
+	// 	setEnteredGoal(enteredText)
+	// }
+
+	const goalInputHandler = (enteredText) => {
+		// console.log(enteredText)
+		setEnteredGoal(enteredText);
+	}
+
+	const addGoalHandler = () => {
+		console.log(enteredGoal)
+		// // simple way, but previous State not always guaranteed
+		// setCourseGoals([...courseGoals, enteredGoal]);
+
+		// better way, previous state guaranteed
+		setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+	}
 
 
-    </View>
-  );
+	return (
+		<View style = {styles.screen}>
+			<View style = {styles.topContainer}>
+				<TextInput
+					placeholder = "Course Goal"
+					style = {styles.input} 
+					onChangeText = {goalInputHandler}
+					value = {enteredGoal}
+				/>
+				<Button 
+					title = "button here"
+					onPress = {addGoalHandler}
+				/>
+			</View>
+			<View>
+				{courseGoals.map((goal, index) => {
+					return (
+						<Text key = {index}> {goal} </Text>
+					)
+				})}
+			</View>
+
+
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        padding: 50,
-    },
-    topContainer: {
-        flexDirection: 'row', 
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    input: {
-        borderColor: 'black', 
-            width: '50%',
-                    borderWidth: 1, 
-                    padding: 10,
-    }
+	screen: {
+		padding: 50,
+	},
+	topContainer: {
+		flexDirection: 'row', 
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	input: {
+		borderColor: 'black', 
+			width: '50%',
+					borderWidth: 1, 
+					padding: 10,
+	}
 
 
 });
 
 
-        // {/* Flexbox exp secion */}
-        // <View 
-        //     style = {{
-        //         margin: 10,
-        //         flexDirection: 'column',// row, column, row/column-reverse
-        //         height: 400,
-        //         width: '60%',
-        //         // aligns children along main-axis of parent
-        //         justifyContent: 'center', // flex-start/end, center, space-between/end/evenly
-        //         // alignItems >> aligns children along Cross-Axis of container
-        //         // for flexDirection: 'column', the Cross-Axis is the horizontal axis 
-        //         alignItems: 'center', // flex-start/end, center, stretch
-        //         // alignContent: 'center',
-        //         borderWidth: 3,
-        //         borderColor: 'deeppink',
-        //         position: 'relative', // default
-        //         borderRadius: 10,
-        //         width: '90%',
-        //         flexWrap: 'wrap',
-        //     }}
-            
-        // >
+		// {/* Flexbox exp secion */}
+		// <View 
+		//     style = {{
+		//         margin: 10,
+		//         flexDirection: 'column',// row, column, row/column-reverse
+		//         height: 400,
+		//         width: '60%',
+		//         // aligns children along main-axis of parent
+		//         justifyContent: 'center', // flex-start/end, center, space-between/end/evenly
+		//         // alignItems >> aligns children along Cross-Axis of container
+		//         // for flexDirection: 'column', the Cross-Axis is the horizontal axis 
+		//         alignItems: 'center', // flex-start/end, center, stretch
+		//         // alignContent: 'center',
+		//         borderWidth: 3,
+		//         borderColor: 'deeppink',
+		//         position: 'relative', // default
+		//         borderRadius: 10,
+		//         width: '90%',
+		//         flexWrap: 'wrap',
+		//     }}
+			
+		// >
 
-        //     <View
-        //         style = {{
-        //             backgroundColor: 'red',
-        //             // width: 100,
-        //             // height: 100,
-        //             flex: 0,
-        //             // justifyContent: 'center',
-        //             // alignItems: 'center',
-        //             margin: 40,
-        //             borderWidth: 3,
-        //             padding: 100,
-        //         }}
-        //     >
-        //         <Text> 1 </Text>
-        //     </View>
+		//     <View
+		//         style = {{
+		//             backgroundColor: 'red',
+		//             // width: 100,
+		//             // height: 100,
+		//             flex: 0,
+		//             // justifyContent: 'center',
+		//             // alignItems: 'center',
+		//             margin: 40,
+		//             borderWidth: 3,
+		//             padding: 100,
+		//         }}
+		//     >
+		//         <Text> 1 </Text>
+		//     </View>
 
-        //     <View
-        //         style = {{
-        //             backgroundColor: 'blue',
-        //             // width: 100,
-        //             // height: 100,
+		//     <View
+		//         style = {{
+		//             backgroundColor: 'blue',
+		//             // width: 100,
+		//             // height: 100,
 
-        //             // amount of space to fill proportional to sum of all children using 'flex'
-        //             flex: 2,
-        //             // aligns children along main-axis of parent
-        //             // justifyContent: 'center', // flex-start/end, center, space-between/end/evenly
-        //             // // alignItems >> aligns children along Cross-Axis of container
-        //             // alignItems: 'center', // flex-start/end, center, stretch
-        //             margin: 5,
-        //             alignSelf: 'flex-start', // flex-start/end, center, stretch
-        //             // alignContent >> like alignItems but ONLY works with items using flexWrap
+		//             // amount of space to fill proportional to sum of all children using 'flex'
+		//             flex: 2,
+		//             // aligns children along main-axis of parent
+		//             // justifyContent: 'center', // flex-start/end, center, space-between/end/evenly
+		//             // // alignItems >> aligns children along Cross-Axis of container
+		//             // alignItems: 'center', // flex-start/end, center, stretch
+		//             margin: 5,
+		//             alignSelf: 'flex-start', // flex-start/end, center, stretch
+		//             // alignContent >> like alignItems but ONLY works with items using flexWrap
 
-        //         }}
-        //     >
-        //         <Text> 2 </Text>
-        //     </View>
+		//         }}
+		//     >
+		//         <Text> 2 </Text>
+		//     </View>
 
-        //     <View
-        //         style = {{
-        //             backgroundColor: 'green',
-        //             // width: 100,
-        //             // height: 150,
-        //             flex: 3,
-        //             justifyContent: 'center',
-        //             alignItems: 'center',
-        //             margin: 5,
-        //         }}
-        //     >
-        //         <Text> 3 </Text>
-        //     </View>
+		//     <View
+		//         style = {{
+		//             backgroundColor: 'green',
+		//             // width: 100,
+		//             // height: 150,
+		//             flex: 3,
+		//             justifyContent: 'center',
+		//             alignItems: 'center',
+		//             margin: 5,
+		//         }}
+		//     >
+		//         <Text> 3 </Text>
+		//     </View>
 
-        //                 <View
-        //         style = {{
-        //             backgroundColor: 'purple',
-        //             // width: 100,
-        //             // height: 100,
-        //             // flex: 3,
-        //             justifyContent: 'center',
-        //             alignItems: 'center',
-        //             margin: 5,
-        //         }}
-        //     >
-        //         <Text> 3 </Text>
-        //     </View>
+		//                 <View
+		//         style = {{
+		//             backgroundColor: 'purple',
+		//             // width: 100,
+		//             // height: 100,
+		//             // flex: 3,
+		//             justifyContent: 'center',
+		//             alignItems: 'center',
+		//             margin: 5,
+		//         }}
+		//     >
+		//         <Text> 3 </Text>
+		//     </View>
 
-        // </View>
+		// </View>
